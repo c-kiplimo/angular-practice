c8eae3f542a5d1404099757640d932ed1f37888f
kiplimo-dev <kiplimocollins855@gmail.com>
2022-12-18 23:17:08 +0300

    added home component

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
   constructor(private router: Router) { }
 
   login(uname: string, pword: string) {
-    if (uname === 'suren' && pword === '1234') {
+    if (uname === 'Collins' && pword === '1234') {
       return 200;
     } else {
       return 403;
