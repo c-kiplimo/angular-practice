3bcff9e56aa987a95b181dac97af66ed60be347e
kiplimo-dev <kiplimocollins855@gmail.com>
2022-12-20 09:02:44 +0300

    added movie component and completed home component

--- src/app/feature/star-rating/star-rating.component.ts
+++ src/app/feature/star-rating/star-rating.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-star-rating',
   templateUrl: './star-rating.component.html',
   styleUrls: ['./star-rating.component.scss']
 })
-export class StarRatingComponent {
+export class StarRatingComponent implements OnInit {
+
+  @Input() rating: any;
+  @Input() isReadOnly: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,7 +8,9 @@ import {HttpClient} from '@angular/common/http'
 })
 export class HomeComponent implements OnInit {
   trendingMovies: any
-  constructor(private http: HttpClient) {}
+  theatreMovies: any;
+  popularMovies: any;
+  constructor(private http: HttpClient,private router: Router) {}
   ngOnInit(): void {
     this.getTrendingMovies();
     
@@ -17,6 +20,19 @@ export class HomeComponent implements OnInit {
       this.trendingMovies=movies;
     });
   
+  }
+  getTheatreMovies(){
+    this.http.get('http://localhost:4200/assets/data/theatre-movies.json').subscribe((movies)=> {
+      this.theatreMovies=movies;
+    });
+  }
+  getPopularMovies(){
+    this.http.get('http://localhost:4200/assets/data/popular-movies.json').subscribe((movies)=> {
+      this.popularMovies=movies;
+    });
+  }
+  goToMovie(type: string,id: string){
+this.router.navigate(['movie',type,id])
   }
 
 }
